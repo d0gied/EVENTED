1640858a9828a1748106f65a1605efd60aaf371d
Nikolay Khadzakos <hadzakos@gmail.com>
2024-03-11 00:05:08 +0300

    complired parser

--- services/parsers/hackathons.rf/parser.py
+++ services/parsers/hackathons.rf/parser.py
@@ -16,13 +16,13 @@ from fake_useragent import UserAgent
 LINK = 'https://www.xn--80aa3anexr8c.xn--p1ai/'
 
 class Hackathon(BaseModel):
-    title: str
-    image: str
-    date_time: str
-    stack: str
-    place: str
-    registration: str
-    link: str
+    title: str = ''
+    image: str = ''
+    date_time: str = ''
+    focus: str = ''
+    place: str = ''
+    registration: str = ''
+    link: str = ''
 
 def parse_hackathons():
     agent = UserAgent()
@@ -57,15 +57,33 @@ def parse_hackathons():
             break
         hackText = hack.find('div', class_='t776__descr t-descr t-descr_xxs').text
         hackPlace = hackText.split('Хакатон')[0]
-        hackDate = hackText.split('Хакатон: ')[1].split('Регистрация')[0]
-        hackReg = hackText.split('Регистрация: ')[1].split('Организатор')[0]
-        hackStack = hackText.split('Технологический фокус: ')[1].split('Призовой фонд')[0]
+        hackPlace = ''
+        if hackText.find('Хакатон') != -1:
+            hackDate = hackText.split('Хакатон:')[1]
+            if hackDate.find('Регистрация') != -1:
+                hackDate = hackDate.split('Регистрация')[0]
+            else:
+                hackDate = hackDate.split('Организатор')[0]
+        hackReg = ''
+        if (hackText.find('Регистрация') != -1):
+            hackReg = hackText.split('Регистрация:')[1].split('Организатор')[0]
+        hackFocus = ''
+        if (hackText.find('Технологический фокус хакатона') != -1):
+            break
+        if hackText.find("Технологический фокус") != -1:
+            hackFocus = hackText.split('Технологический фокус:')[1]
+            if hackFocus.find('Призовой фонд') != -1:
+                hackFocus = hackFocus.split('Призовой фонд')[0]
+            elif hackFocus.find('Целевая аудитория') != -1:
+                hackFocus = hackFocus.split('Целевая аудитория')[0]
         
-        print(hack)
+        print(hackTitle, hackImage, hackDate, hackFocus, hackPlace, hackReg, hackLink, sep='\n', end='\n\n')
         
+        hack = Hackathon(title=hackTitle, image=hackImage, date_time=hackDate, focus=hackFocus, place=hackPlace, registration=hackReg, link=hackLink)
     browser.quit()
 
-schedule.every().day.at("09:00").do(parse_hackathons)
-        
-while True:
-    schedule.run_pending()
+# schedule.every().day.at("09:00").do(parse_hackathons)
+# while True:
+    # schedule.run_pending()
+
+parse_hackathons()
